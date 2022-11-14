317b53b6fce07a18ec49aa2db5c18f4e02cd9929
GeoSauer <geoffrey.sauer89@gmail.com>
2022-11-14 13:49:19 -0700

    cleanup pass deleting .skip from early test suites

--- __tests__/aliens.test.js
+++ __tests__/aliens.test.js
@@ -3,7 +3,7 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe.skip('aliens routes', () => {
+describe('aliens routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
--- __tests__/rangers.test.js
+++ __tests__/rangers.test.js
@@ -3,7 +3,7 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe.skip('rangers routes', () => {
+describe('rangers routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
